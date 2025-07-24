cbfdc631d732b4e22518c25777353125f1ace9d4
junah201 <junah.dev@gmail.com>
2025-07-24 13:14:58 +0900

    Remove unnecessary file and repository

--- src/main/java/dev/junah/spring_study/controller/UserController.java
+++ src/main/java/dev/junah/spring_study/controller/UserController.java
@@ -13,8 +13,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
--- src/main/java/dev/junah/spring_study/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.junah.spring_study.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dev.junah.spring_study.domain.Board;
-
-public interface BoardRepository extends JpaRepository<Board, String> {
-}
